38f3f0bde89adafae6127fd3bdc4499950d50276
bwilsoncc <bwilsoncc@co.clatsop.or.us>
2019-12-19 11:37:35 -0800

    commented

--- create_databases.sh
+++ create_databases.sh
@@ -1,4 +1,12 @@
+#!env bash
+#
+# Read the database names from all the YAML files here,
+# find database names, and create CouchDB databases for them.
+# (If a database already exists, ignore it and press on.)
+
+# Get username and password
 source .env
+
 for SVC in `grep db_name: *yaml | sed -e 's/.*db_name: //g'`
 do
     echo $SVC
