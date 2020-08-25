8edde228566f9a68d23fb390034d537e3ced516f
Brian Wilson <brian@wildsong.biz>
2020-08-25 13:28:10 -0700

    allow multi or single service via config

--- start_mapproxy.py
+++ start_mapproxy.py
@@ -4,7 +4,14 @@ import os
 from logging.config import fileConfig
 
 logfile = sys.argv[1]
-config  = sys.argv[2]
+
+try:
+    config  = sys.argv[2]
+except:
+    raise Exception("Config not found on command line.")
+if not os.path.exists(config):
+    raise Exception("Missing config file or directory")
+multiservice = os.path.isdir(config)
 
 fileConfig(logfile, {'here': os.path.dirname(__file__)})
 
@@ -13,13 +20,16 @@ try:
 except:
     myport=8080
 
-# This version starts the multi service, config should be a directory.
-# note that reloader option is not available here
-from mapproxy.multiapp import make_wsgi_app
-application = make_wsgi_app(config, allow_listing = True)
-
-# This version starts just one, config should be a mapproxy.yaml file.
-#from mapproxy.wsgiapp import make_wsgi_app
-#application = make_wsgi_app(config, reloader = True)
+if os.path.isdir(config):
+    # The config is a directory, so
+    # the MapProxy will start as a multi service
+    # note that reloader option is not available here
+    from mapproxy.multiapp import make_wsgi_app
+    application = make_wsgi_app(config, allow_listing = True)
+else:
+    # For a simple service,
+    # config should be a mapproxy.yaml file.
+    from mapproxy.wsgiapp import make_wsgi_app
+    application = make_wsgi_app(config, reloader = True)
 
 waitress.serve(application, threads=16, host='0.0.0.0', port=myport)
